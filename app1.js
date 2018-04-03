b684b10e7143a02ee5e5c1433a5bf8b7b73de86e
sihemgue <guemri.sihem@gmail.com>
2018-04-03 17:06:33 +0200

    add message

--- app1.js
+++ app1.js
@@ -9,11 +9,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/contact', function (req, res) {
-    res.send('plz ....')
 
-    console.log(req.body);
-  })
   app.post('/', function (req, res) {
     if(req.body.pass === '123'){
         res.send({message: 'welcome'+ req.body.login})
